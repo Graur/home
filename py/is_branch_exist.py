068c8654277e21a38f11e86965c35b06f1e36fbe
graur <ancongr@yandex.ru>
2023-08-10 11:28:39 +0300

    changed condition

--- py/is_branch_exist.py
+++ py/is_branch_exist.py
@@ -11,5 +11,5 @@ result = subprocess.run(command, shell=True, capture_output=True)
 is_exist = result.returncode == 0
 env_file = os.getenv('GITHUB_ENV')
 with open(env_file, "a") as myfile:
-    myfile.write(f'is_exist={is_exist}')
+    myfile.write(f'is_exist={"true" if is_exist else "false"}\n')
 print(f'Added to env: {is_exist}')
